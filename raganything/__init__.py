9f9fb6801070c03c37f4e9c6db2d9d3ea4d04a8b
zrguo <49157727+LarFii@users.noreply.github.com>
2025-07-22 02:25:00 +0800

    Update __init__.py

--- raganything/__init__.py
+++ raganything/__init__.py
@@ -1,7 +1,7 @@
 from .raganything import RAGAnything as RAGAnything
 from .config import RAGAnythingConfig as RAGAnythingConfig
 
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 __author__ = "Zirui Guo"
 __url__ = "https://github.com/HKUDS/RAG-Anything"
 
